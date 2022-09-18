0eae0ebfd858d53b145524af624f3e7226799510
Alibek <alibek@pop-os.localdomain>
2022-09-18 15:35:47 +0500

    a

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("api/", include("djoser.urls")),
     path("api/auth/", include("djoser.urls.authtoken")),
     path(
-        'api/redoc/',
+        'api/docs/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
