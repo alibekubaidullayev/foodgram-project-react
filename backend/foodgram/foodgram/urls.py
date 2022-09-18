10ecd259d8db704c0d84de622877b147e21e8717
Alibek <alibek@pop-os.localdomain>
2022-09-18 16:20:45 +0500

    a

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -2,18 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("api/", include("djoser.urls")),
     path("api/auth/", include("djoser.urls.authtoken")),
-    path(
-        'api/docs/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
 ]
 
 
